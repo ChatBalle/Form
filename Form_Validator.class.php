ae240c861c5197bab57069968bfb7ce6e55fc06c
christophetd <christophetd@gmail.com>
2010-03-09 17:45:44 +0100

    Correction d'un nom de fichier errone

--- Form_Validator.class.php
+++ Form_Validator.class.php
@@ -1,13 +1,13 @@
-﻿<?php
-
+<?php
+ 
 abstract class Form_Validator {
-
+ 
 	protected $_options = array();
 	protected $_error_message = '';
 	static $required_options = array();
-
+ 
 	public function __construct(array $options, $error_message) {
-	
+ 
 		$missing_options = array_diff(static::$required_options, array_keys($options));
 		if (!empty($missing_options)) {
 			throw new Exception(sprintf(%.': the following options are missing: %s',
@@ -17,10 +17,10 @@ abstract class Form_Validator {
 		$this->_options       = $options;
 		$this->_error_message = $error_message;
 	}
-	
+ 
 	function is_valid($field) {
 		return test($field->getAttribute('value'), $this->_options);
 	}
-	
+ 
 	abstract static function test($value, $options);
 }
